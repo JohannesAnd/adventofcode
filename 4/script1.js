3049bbfa7e4fdb78fe70c5cdde6f860bc9081003
Johannes Andersen <johanes1@online.no>
2017-12-04 11:43:37 +0100

    Day 4

---
+++ 4/script1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf-8');
+
+const isValid = p =>
+  Object.keys(
+    p
+      .split(' ')
+      .reduce((a, w) => Object.defineProperty(a, w, { enumerable: true }), {})
+  ).length === p.split(' ').length;
+
+const result = input.split('\n').reduce((s, v) => s + isValid(v), 0);
+
+console.log(result);
