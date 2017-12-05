14abfb6954f3a66645132e960169912468d52aba
Johannes Andersen <johanes1@online.no>
2017-12-05 19:18:05 +0100

    Added day 3 part 2

--- 3/script2.js
+++ 3/script2.js
@@ -1,9 +1,37 @@
 const input = 289326;
 
-let number = 1;
+const getValue = (matrix, posX, posY) => {
+  let sum = 0;
 
-while (number < input) {
-  // Do stuff
+  for (var x = posX - 1; x <= posX + 1; x++) {
+    for (var y = posY - 1; y <= posY + 1; y++) {
+      if (matrix[x + ',' + y]) {
+        sum += matrix[x + ',' + y];
+      }
+    }
+  }
+  return sum;
+};
+
+let x = 0;
+let y = 0;
+let matrix = {};
+
+matrix[x + ',' + y] = 1;
+
+while (true) {
+  const val = getValue(matrix, x, y);
+
+  if (val >= input) {
+    console.log(val);
+    break;
+  }
+
+  matrix[x + ',' + y] = val;
+
+  if ((x !== y || x >= 0) && Math.abs(x) <= Math.abs(y)) {
+    x += y >= 0 ? 1 : -1;
+  } else {
+    y += x >= 0 ? -1 : 1;
+  }
 }
-
-console.log(number);
