0ee54704d5383f4a4b4b46d9a06cc2358c3686da
Johannes Andersen <johanes1@online.no>
2017-12-12 17:26:11 +0100

    Previous

--- previous/2015/9/script2.js
+++ previous/2015/9/script2.js
@@ -2,20 +2,64 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./input.txt', 'utf-8');
 
-const computeDiff = s => {
-  const newString = s
-    .substring(0)
-    .replace(/\\/g, '\\\\')
-    .replace(/"/g, '\\"');
+const { distances, cities } = input.split('\n').reduce(
+  (a, e) => {
+    const [from, , to, , distance] = e.split(' ');
 
-  console.log(s, newString);
+    if (from in a.distances) {
+      a.distances[from][to] = Number(distance);
+    } else {
+      a.distances[from] = {
+        [to]: Number(distance)
+      };
+    }
 
-  return newString.length + 2 - s.length;
+    if (to in a.distances) {
+      a.distances[to][from] = Number(distance);
+    } else {
+      a.distances[to] = {
+        [from]: Number(distance)
+      };
+    }
+
+    a.cities[from] = true;
+    a.cities[to] = true;
+
+    return a;
+  },
+  { distances: {}, cities: {} }
+);
+
+const getRoute = (prev = []) => {
+  const remaining = Object.keys(cities).filter(c => prev.indexOf(c) === -1);
+
+  if (remaining.length === 0) return [prev];
+
+  return remaining
+    .map(city => getRoute(prev.concat([city])))
+    .reduce((a, d) => a.concat(d), []);
 };
 
-const result = input
-  .split('\n')
-  .map(computeDiff)
-  .reduce((s, v) => s + v, 0);
+const result = getRoute()
+  .map(route => {
+    let distance = 0;
 
-console.log(result);
+    for (let i = 0; i < route.length - 1; i++) {
+      const from = route[i];
+      const to = route[i + 1];
+
+      if (from in distances) {
+        if (to in distances[from]) {
+          distance += distances[from][to];
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    return distance;
+  })
+  .filter(d => d !== false);
+
+console.log(Math.max(...result));
