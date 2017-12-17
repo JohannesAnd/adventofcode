71d8cb91ab9f808bc0ae0f0009ef089a75d720ff
Johannes Andersen <johanes1@online.no>
2017-12-17 19:05:06 +0100

    Optimized day 17 part 2

--- 17/script2.js
+++ 17/script2.js
@@ -4,17 +4,14 @@ const step = 366;
 
 let pointer = 0;
 
-let zeroPointer = 0;
 let afterZeroValue = false;
 
 for (let i = 1; i <= 50000000; i++) {
   pointer = (pointer + step) % i + 1;
 
-  if (pointer < zeroPointer) {
-    zeroPointer++;
-  } else if (pointer === zeroPointer + 1) {
+  if (pointer === 1) {
     afterZeroValue = i;
   }
 }
 
-console.log(afterZeroValue);
+console.log(afterZeroValue, zeroPointer);
