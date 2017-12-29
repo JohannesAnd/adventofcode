677ed536263597ff54ff23fe61b159c11a986100
Johannes Andersen <johanes1@online.no>
2017-12-29 13:05:31 +0100

    Day 22 part 2

--- 22/script2.js
+++ 22/script2.js
@@ -1,1 +1,64 @@
-// Todo!
+const fs = require('fs');
+
+const mod4 = n => (n % 4 + 4) % 4;
+
+const input = fs
+  .readFileSync('./input.txt', 'utf-8')
+  .split('\n')
+  .map(l => l.split(''));
+
+const nodes = {};
+const bursts = 10000000;
+
+let x = Math.floor(input.length / 2);
+let y = Math.floor(input[0].length / 2);
+
+let infections = 0;
+let dir = 0;
+
+for (let yi = 0; yi < input.length; yi++) {
+  for (let xi = 0; xi < input[yi].length; xi++) {
+    if (input[yi][xi] === '#') {
+      nodes[JSON.stringify({ x: xi, y: yi })] = 2;
+    }
+  }
+}
+
+for (let i = 0; i < bursts; i++) {
+  switch (nodes[JSON.stringify({ x, y })]) {
+    case 0:
+    case undefined:
+      dir = mod4(dir - 1);
+      nodes[JSON.stringify({ x, y })] = 1;
+      break;
+    case 1:
+      nodes[JSON.stringify({ x, y })] = 2;
+      infections++;
+      break;
+    case 2:
+      dir = mod4(dir + 1);
+      nodes[JSON.stringify({ x, y })] = 3;
+      break;
+    case 3:
+      dir = mod4(dir + 2);
+      nodes[JSON.stringify({ x, y })] = 0;
+      break;
+  }
+
+  switch (dir) {
+    case 0:
+      y -= 1;
+      break;
+    case 1:
+      x += 1;
+      break;
+    case 2:
+      y += 1;
+      break;
+    case 3:
+      x -= 1;
+      break;
+  }
+}
+
+console.log(infections);
--- 23/script2.js
+++ 23/script2.js
@@ -1,1 +1,36 @@
-// TODO
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf-8');
+
+const instructions = input.split('\n');
+
+const registers = { a: 1, b: 0, c: 0, e: 0, f: 0, g: 0, h: 0 };
+
+let pointer = 0;
+
+while (pointer >= 0 && pointer < instructions.length) {
+  const [op, arg1, arg2] = instructions[pointer].split(' ');
+
+  const val1 = arg1 in registers ? registers[arg1] : Number(arg1);
+  const val2 = arg2 in registers ? registers[arg2] : Number(arg2);
+
+  switch (op) {
+    case 'set':
+      registers[arg1] = val2;
+      pointer++;
+      break;
+    case 'sub':
+      registers[arg1] -= val2;
+      pointer++;
+      break;
+    case 'mul':
+      registers[arg1] *= val2;
+      pointer++;
+      break;
+    case 'jnz':
+      pointer += val1 === 0 ? 1 : val2;
+      break;
+  }
+}
+
+console.log(registers.h);
